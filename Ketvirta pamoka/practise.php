7f6f116a273def086ee94869460a187fc25603fd
ErnestZaiko <ernest.zaiko@gmail.com>
2022-01-04 23:14:27 +0300

    Added new codes

--- Ketvirta pamoka/practise.php
+++ Ketvirta pamoka/practise.php
@@ -4,7 +4,19 @@ $name = 'Ernest';
 $surname = 'Zaiko';
 
 echo getNickName($name, $surname);
+echo "<br>";
 
 function getNickName($name, $surname){
-    return strtolower(substr($name, 0, 3).substr($surname, 0, 3));
+    return strtolower(substr($name, 0, 3).substr($surname, 0, 3)).mt_rand(1, 100);
 }
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+$title = "Labas rytas siandien yra salta lauke";
+echo getSlug($title);
+function getSlug($title) {
+    return strtolower(str_replace(" ", "-", $title));
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
