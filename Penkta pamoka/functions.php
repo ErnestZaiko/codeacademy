5aeafedc7a7d5afd6d4a8912be607dfbefa4c569
ErnestZaiko <ernest.zaiko@gmail.com>
2022-01-06 00:10:36 +0300

    HTML starting pack

---
+++ Penkta pamoka/functions.php
@@ -0,0 +1,99 @@
+<?php
+
+
+// $email = $_POST['email'];
+
+// function isEmailValid($email)
+// {
+//     if(strpos($email, '@')) {
+//         return true;
+//     } else {
+//             return false;
+//     }
+// }
+// if(isEmailValid($email))
+// {
+//     echo 'Valid email';
+// } else {
+//     echo 'Invalid email';
+// }
+
+//////////////////////////////////////////////////////////////////
+
+// if($_POST) {
+//     echo $_POST["skaicius1"] + $_POST["skaicius2"];
+// }
+
+//////////////////////////////////////////////////////////////////
+
+// if($_POST) {
+//     if($_POST["veiksmas"] == "+")
+//         echo $_POST["skaicius1"] + $_POST["skaicius2"];
+//     if($_POST["veiksmas"] == "-")
+//         echo $_POST["skaicius1"] - $_POST["skaicius2"];
+//     if($_POST["veiksmas"] == "*")
+//         echo $_POST["skaicius1"] * $_POST["skaicius2"];
+//     if($_POST["veiksmas"] == "/")
+//         echo $_POST["skaicius1"] / $_POST["skaicius2"];
+// }
+
+//////////////////////////////////////////////////////////////////
+
+
+$name = $_POST['name'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirmPassword = $_POST['confirm_password'];
+function clearEmail($email)
+{
+    return trim(strtolower($email));
+}
+function isEmailValid($email)
+{
+    if (strpos($email, '@')) {
+        return true;
+    } else {
+        echo 'Invalid email';
+    }
+}
+function getNickName($name, $lastname)
+{
+    return trim(strtolower(substr($name, 0, 3) . substr($lastname, 0, 3)) . rand(1, 100));
+}
+function isPasswordValid($password, $confirmPassword)
+{
+    if (strlen($password) > 5) {
+        if ($password == $confirmPassword) {
+            return true;
+        } else {
+            echo 'Password and Confirm password should match!';
+        }
+    } else {
+        echo 'Password must be at least 6 charecters long';
+    }
+}
+function register($name, $lastname, $email, $password, $confirmPassword)
+{
+    if (isEmailValid(clearEmail($email))) {
+        if (isPasswordValid($password, $confirmPassword)) {
+            echo "Welcome " . $name . " " . $lastname;
+            echo "<br>" . "Your email is: " . $email;
+            echo "<br>" . "Your Username is: " . getNickName($name, $lastname);
+        }
+    }
+}
+$data = $_POST;
+
+if (empty($data['name']) ||
+    empty($data['lastname']) ||
+    empty($data['password']) ||
+    empty($data['email']) ||
+    empty($data['confirm_password'])) {
+    
+    die('Please fill all required fields!');
+
+}
+register($name, $lastname, $email, $password, $confirmPassword);
+
+//////////////////////////////////////////////////////////////////
---
+++ Penkta pamoka/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Formos</title>
+    </head>
+    <body>
+        <div class="header">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Map</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <h1>Register form</h1>
+            <p>Please complete register form</p>
+
+            <!-- <form action="functions.php" method="post">
+                <input type="text" name="user_email" placeholder="ernest.zaiko@gmail.com">
+                <input type="submit" value="OK" name="create" />
+            </form>
+
+            <br>
+
+            <form action="functions.php" method="post">
+                <input type="number" name="skaicius1">
+                <input type="number" name="skaicius2">
+                <input type="submit" value="OK" name="submit">
+            </form>
+
+            <br>
+
+            <form action="functions.php" method="post">
+                <input type="number" name="skaicius1">
+                <select name="veiksmas">
+                    <option value="*">*</option>
+                    <option value="-">-</option>
+                    <option value="+">+</option>
+                    <option value="/">/</option>
+                </select>
+                <input type="number" name="skaicius2">
+                <input type="submit" value="OK" name="submit">
+            </form>
+
+            <br> -->
+
+            <form action="functions.php" method="POST">
+                Name: <input type="text" name="name" autocomplete="off"/><br/>
+                Lastname: <input type="text" name="lastname" autocomplete="off"/><br/>
+                Email: <input type="text" name="email" autocomplete="off"/><br/>
+                Password: <input type="text" name="password" autocomplete="off"/><br/>
+                Confirm password: <input type="text" name="confirm_password" autocomplete="off"/><br/>
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    </body>
+</html>
+
+
+
+
+
+
+
+
+<style>
+    .header{background: #0000ff; color: #ccc; border-bottom: 1px solid;}
+    .header ul{display: flex; flex-wrap: wrap;}
+    .header li {margin-left: 20px;}
+    .header a {color:#ff0000; text-decoration: none;}
+    .content{width: 800px; margin: 0 auto; background: beige;}
+</style>
