79aece9e81d66fd36c21a28a0f558dffa0ec0499
ErnestZaiko <ernest.zaiko@gmail.com>
2022-02-03 20:55:12 +0300

    Added new version OOP

---
+++ OOP2/app/code/Controller/Catalog.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Controller;
+
+class Catalog
+{
+    public function show($id = null)
+    {
+        if ($id !== null) {
+            echo 'Catalog controller ID ' . $id;
+        }
+    }
+
+    public function all()
+    {
+        for ($i = 0; $i < 10; $i++) {
+            echo '<a href="http://127.0.0.1:8001/index.php/catalog/show/' . $i . '">Read more</a>';
+            echo '<br>';
+        }
+    }
+
+    public function create($data)
+    {
+
+    }
+
+    public function update($data)
+    {
+        echo 'I\'m Robot';
+    }
+}
+
---
+++ OOP2/app/code/Controller/User.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Controller;
+
+use Helper\DBHelper;
+use Helper\FormHelper;
+use Helper\Validator;
+use Helper\Url;
+use Model\City;
+use Model\User as UserModel;
+
+class User
+{
+    public function show($id)
+    {
+        echo 'User controller ID: ' . $id;
+    }
+
+    public function register()
+    {
+
+        $form = new FormHelper('user/create', 'POST');
+
+        $form->input([
+            'name' => 'name',
+            'type' => 'text',
+            'placeholder' => 'Vardas'
+        ]);
+        $form->input([
+            'name' => 'last_name',
+            'type' => 'text',
+            'placeholder' => 'Pavarde'
+        ]);
+        $form->input([
+            'name' => 'phone',
+            'type' => 'text',
+            'placeholder' => '+3706*******'
+        ]);
+        $form->input([
+            'name' => 'email',
+            'type' => 'email',
+            'placeholder' => 'Email'
+        ]);
+        $form->input([
+            'name' => 'password',
+            'type' => 'password',
+            'placeholder' => '* * * * * *'
+        ]);
+        $form->input([
+            'name' => 'password2',
+            'type' => 'password',
+            'placeholder' => '* * * * * *'
+        ]);
+
+        $cities = City::getCities();
+        $options = [];
+        foreach ($cities as $city) {
+            $id = $city->getId();
+            $options[$id] = $city->getName();
+        }
+        $form->select(['name' => 'city_id', 'options' => $options]);
+        $form->input([
+            'name' => 'create',
+            'type' => 'submit',
+            'value' => 'register'
+        ]);
+
+        echo $form->getForm();
+    }
+
+    public function edit()
+    {
+        if (!isset($_SESSION['user_id'])) {
+            Url::redirect('user/login');
+        }
+
+        $userId = $_SESSION['user_id'];
+        $user = new UserModel();
+        $user->load($userId);
+
+        $form = new FormHelper('user/update', 'POST');
+        $form->input([
+            'name' => 'name',
+            'type' => 'text',
+            'placeholder' => 'Vardas',
+            'value' => $user->getName()
+        ]);
+
+        $form->input([
+            'name' => 'last_name',
+            'type' => 'text',
+            'placeholder' => 'Pavarde',
+            'value' => $user->getLastName()
+        ]);
+        $form->input([
+            'name' => 'phone',
+            'type' => 'text',
+            'placeholder' => '+3706*******',
+            'value' => $user->getPhone()
+        ]);
+        $form->input([
+            'name' => 'email',
+            'type' => 'email',
+            'placeholder' => 'Email',
+            'value' => $user->getEmail()
+        ]);
+        $form->input([
+            'name' => 'password',
+            'type' => 'password',
+            'placeholder' => '* * * * * *'
+        ]);
+        $form->input([
+            'name' => 'password2',
+            'type' => 'password',
+            'placeholder' => '* * * * * *'
+        ]);
+
+        $cities = City::getCities();
+        $options = [];
+        foreach ($cities as $city) {
+            $id = $city->getId();
+            $options[$id] = $city->getName();
+        }
+        $form->select([
+            'name' => 'city_id',
+            'options' => $options,
+            'selected' => $user->getCityId()
+        ]);
+
+        $form->input([
+            'name' => 'create',
+            'type' => 'submit',
+            'value' => 'register'
+        ]);
+
+        echo $form->getForm();
+
+
+    }
+
+    public function login()
+    {
+        $form = new FormHelper('user/check', 'POST');
+        $form->input([
+            'name' => 'email',
+            'type' => 'email',
+            'placeholder' => 'Email'
+        ]);
+        $form->input([
+            'name' => 'password',
+            'type' => 'password',
+            'placeholder' => '* * * * * *'
+        ]);
+        $form->input([
+            'name' => 'login',
+            'type' => 'submit',
+            'value' => 'login'
+        ]);
+
+        echo $form->getForm();
+    }
+
+    public function create()
+    {
+        $passMatch = Validator::checkPassword($_POST['password'], $_POST['password2']);
+        $isEmailValid = Validator::checkEmail($_POST['email']);
+        $isEmailUnic = UserModel::emailUnic($_POST['email']);
+        if ($passMatch && $isEmailValid && $isEmailUnic) {
+            $user = new UserModel();
+            $user->setName($_POST['name']);
+            $user->setLastName($_POST['last_name']);
+            $user->setPhone($_POST['phone']);
+            $user->setPassword(md5($_POST['password']));
+            $user->setEmail($_POST['email']);
+            $user->setCityId($_POST['city_id']);
+            $user->save();
+            Url::redirect('user/login');
+        } else {
+            echo 'Patikrinkite duomenis';
+        }
+    }
+
+    public function check()
+    {
+        $email = $_POST['email'];
+        $password = md5($_POST['password']);
+        $userId = UserModel::checkLoginCredentionals($email, $password);
+        if ($userId) {
+            $user = new UserModel();
+            $user->load($userId);
+            $_SESSION['logged'] = true;
+            $_SESSION['user_id'] = $userId;
+            $_SESSION['user'] = $user;
+            Url::redirect('/');
+        } else {
+            Url::redirect('user/login');
+        }
+    }
+
+    public function logout()
+    {
+        session_destroy();
+    }
+
+}
