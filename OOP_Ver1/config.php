87754aa808a11b7f9aa393ba9025290958f7f523
ErnestZaiko <97046670+ErnestZaiko@users.noreply.github.com>
2022-02-28 23:26:04 +0300

    Delete config.php

--- OOP_Ver1/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('BASE_URL', 'http://127.0.0.1:8000/');
-define('SERVERNAME', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'hackme');
-define('DB_NAME', 'shop_lt');
